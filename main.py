943cedb7eb80635c5cd0138b6e1c5cb46c740d3a
Jameson <jamesonlyon@users.noreply.github.com>
2025-02-12 14:36:50 -0800

    Add word_count func and return

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def word_count(string):
+    words = string.split()
+    count = len(words)
+    return count
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read() # get the contents of the file
+        words = word_count(file_contents) # get the word count
+        print(words)
+        return words
+
+main()
