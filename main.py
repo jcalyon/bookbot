f7e08cc8e466a4df5aaff74dfe7789791b0b22bd
Jameson <jamesonlyon@users.noreply.github.com>
2025-02-12 14:46:13 -0800

    Add char_freq and print test

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ def word_count(string):
     count = len(words)
     return count
 
+def char_freq(string):
+    char_dict = {}
+    for char in string:
+        char = char.lower()
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read() # get the contents of the file
         words = word_count(file_contents) # get the word count
-        print(words)
+        chars = char_freq(file_contents) # calc the char frequency
+        print(chars)
         return words
 
 main()
